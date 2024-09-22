f0e96722cb1a9c0b955b5b3b7e57fba80f73dfdc
cpreet <cpreet.chawla@gmail.com>
2024-09-22 13:54:12 +0530

    feat: added theme provider and changing component to the FE

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { lazy } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import { ThemeProvider } from '@/components/ThemeProvider'
 import { Card } from '@/components/ui/card'
 const Login = lazy(() => import('@/components/Login'))
 // import Login from '@/components/Login'
@@ -25,9 +26,11 @@ const router = createBrowserRouter([
 
 function App() {
   return (
+    <ThemeProvider defaultTheme='light' storageKey='ui-theme'>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
+    </ThemeProvider>
   )
 }
 
